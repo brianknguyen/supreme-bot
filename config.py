f4164da04d135653cd7cf69205ce28f84e533c9e
brianknguyen <42389782+brianknguyen@users.noreply.github.com>
2020-08-10 00:01:23 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+
+
+
+keys = {
+    "product_url": "https://www.supremenewyork.com/shop/jackets/i2k091dbl",
+    "name": "Brian Nguyen",
+    "email": "hahaha@gmail.com",
+    "phone_number": "5555555555",
+    "address": "323 Forrest Drive",
+    "zip": 95616,
+    "card_number": 5555555555555555,
+    "card_cvv": 555,
+    "card_exp_mo": 12,
+    "card_exp_yr": 3
+}
